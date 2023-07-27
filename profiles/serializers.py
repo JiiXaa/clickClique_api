05261249e837c5db901219cb744e14f97636bf7a
JiiXaa <anythingfx@gmail.com>
2023-07-27 21:56:45 +0100

    added login/logout views, IsOwner permission and the is_owner field to ProfileSerializer.

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    # is_owner is a custom field that returns True if the user is the owner of the profile. That will help on the frontend to show the edit and delete buttons only to the owner of the profile
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        # To get the user, we use self.context["request"].user instead of self.context["request"].data["owner"]
+        # The request context is passed to the serializer by the view
+        request = self.context["request"]
+        return obj.owner == request.user
 
     class Meta:
         model = Profile
@@ -15,4 +23,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             "name",
             "content",
             "image",
+            "is_owner",
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,14 +2,16 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from .serializers import ProfileSerializer
 from .models import Profile
+from cc_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        data = ProfileSerializer(profiles, many=True).data
+        data = ProfileSerializer(profiles, many=True, context={"request": request}).data
         return Response(data)
 
     def post(self, request):
@@ -23,22 +25,28 @@ class ProfileList(APIView):
 class ProfileDetail(APIView):
     # serializer_class set to ProfileSerializer renders form with pre-populated fields in the template instead of RAW data
     serializer_class = ProfileSerializer
+    # permission_classes set to IsOwnerOrReadOnly to allow only the owner of the profile to edit it
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            # This line checks if the user is the owner of the profile
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        data = ProfileSerializer(profile).data
+        data = ProfileSerializer(profile, context={"request": request}).data
         return Response(data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={"request": request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
