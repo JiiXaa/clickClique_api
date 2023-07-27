d7425dff9abb7ef840701be3b2544467b5ab3569
JiiXaa <anythingfx@gmail.com>
2023-07-27 21:39:11 +0100

    added profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProfileSerializer
@@ -16,3 +18,28 @@ class ProfileList(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class ProfileDetail(APIView):
+    # serializer_class set to ProfileSerializer renders form with pre-populated fields in the template instead of RAW data
+    serializer_class = ProfileSerializer
+
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        data = ProfileSerializer(profile).data
+        return Response(data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
