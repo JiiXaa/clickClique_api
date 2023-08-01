6643914a3b7deee499871146b388ac18c4567ffd
JiiXaa <anythingfx@gmail.com>
2023-08-01 21:23:07 +0100

    Created LikeList and LikeDetail generic views, CRUD functionality.

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from cc_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes, or create a new like.
+    """
+
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a like instance.
+    """
+
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
