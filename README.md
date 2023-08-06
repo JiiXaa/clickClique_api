664b0d5840556e58c59d6a9f15cf2158d4687b7d
JiiXaa <anythingfx@gmail.com>
2023-08-06 22:20:38 +0100

    Added JSON Web Tokens to the project and configured the app.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # clickClique_api
 
 ## Common usage patterns using Django's generic views to avoid repetition for the views.
+
+## dj-rest-auth API endpoints:
+
+| url                        | http | data sent                    | data received                     |
+| -------------------------- | ---- | ---------------------------- | --------------------------------- |
+| dj-rest-auth/registration  | POST | username password1 password2 | -                                 |
+| dj-rest-auth/login         | POST | username password            | access token refresh token        |
+| dj-rest-auth/logout        | POST | -                            | -                                 |
+| dj-rest-auth/user/         | GET  | access token refresh token   | username profile_id profile_image |
+| dj-rest-auth/token/refresh | POST | access token refresh token   | (new) access token                |
--- cc_api/settings.py
+++ cc_api/settings.py
@@ -23,6 +23,28 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# DRF authentication: tokens not supported for the Browsable API. Session used in development token used in production this set up allows us to use both. Check the DEV value in env.py
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        (
+            "rest_framework.authentication.SessionAuthentication"
+            if "DEV" in os.environ
+            else "dj_rest_auth.jwt_auth.JWTCookieAuthentication"
+        )
+    ]
+}
+
+REST_USE_JWT = True
+# auth secure is set to true to ensure that the cookie is only sent over HTTPS
+JWT_AUTH_SECURE = True
+# Access token
+JWT_AUTH_COOKIE = "cc_api_auth"
+# Refresh token
+JWT_AUTH_REFRESH_COOKIE = "cc_api_auth_refresh"
+
+REST_AUTH_SERIALIZERS = {
+    "USER_DETAILS_SERIALIZER": "cc_api.serializers.CurrentUserSerializer"
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -49,12 +71,20 @@ INSTALLED_APPS = [
     "cloudinary",
     "rest_framework",
     "django_filters",
+    "rest_framework.authtoken",
+    "dj_rest_auth",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth.registration",
     "profiles",
     "posts",
     "comments",
     "likes",
     "followers",
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
